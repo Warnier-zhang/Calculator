8331c903e93d44303e4004e6a8938372b39881f7
Warnier-zhang <warnier.zhang@gmail.com>
2016-05-22 20:32:36 +0800

    refactor(BundleKey): Adopt enumeration for data key

---
+++ src/org/warnier/zhang/calculator/model/BundleKey.java
@@ -0,0 +1,11 @@
+package org.warnier.zhang.calculator.model;
+
+public class BundleKey {
+    public static final String S = "S";
+    public static final String O = "operator";
+    public static final String E = "E";
+
+    private BundleKey() {
+
+    }
+}
