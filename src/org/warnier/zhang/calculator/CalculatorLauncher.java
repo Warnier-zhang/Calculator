e4eb85d4eaefc5a59e25f7064cac874351ee45d8
Warnier-zhang <warnier.zhang@gmail.com>
2016-05-22 12:58:50 +0800

    feature(project): Set start point for Calculator

---
+++ src/org/warnier/zhang/calculator/CalculatorLauncher.java
@@ -0,0 +1,19 @@
+package org.warnier.zhang.calculator;
+
+import javax.swing.*;
+
+/**
+ * The main launcher for Calculator.
+ * @author Warnier-zhang
+ * @version 0.1
+ */
+public class CalculatorLauncher {
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+    }
+}
